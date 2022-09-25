e747f0def60fbbc563a27042f9e7d73ee6e0c503
me <me>
2022-09-24 19:19:11 -0700

    proper wrap what a dumb silly

--- conways.py
+++ conways.py
@@ -1,54 +1,31 @@
 # imports
-import enum
 import os
+
+from cell import CellState
+from cell import Cell
+
 from random import randint
-# enum class stuff
-class CellState(enum.Enum):
-    DEAD = 0
-    ALIVE = 1
-
-class Cell():
-    def __init__(self) -> None:
-        self.neighbors = 0
-        self.cellstate = CellState.DEAD
-
-    def inc_neighbors(self):
-        self.neighbors += 1
-
-    def dec_neighbors(self):
-        self.neighbors -= 1
-
-        if self.neighbors < 0:
-            self.neighbors = 0
 
 # helper functions
 def drawScreen(screen: list[list[Cell]]):
     def stuff(row):
-        list(map(lambda cell: print("#" if cell.cellstate == CellState.ALIVE else " ", end=" "), row))
+        list(map(lambda cell: print(cell.neighbors if cell.cellstate == CellState.ALIVE else " ", end=" "), row))
         print()
 
     list(map(lambda row: stuff(row), screen))
 
-def neighboringPositions(x: int, y: int) -> list[tuple[int, int]]:
+def create_screen(width, height):
+    screen = list(map(lambda _: list(map(lambda _: Cell(), range(width))), range(height)))
+    return screen
+
+def neighboring_positions(x: int, y: int) -> list[tuple[int, int]]:
     positions = [
             (x-1, y+1), (x, y+1), (x+1, y+1), 
             (x-1, y),             (x+1, y),
             (x-1, y-1), (x, y-1), (x+1, y-1)]
     return positions
 
-
-def createScreen(width, height):
-    screen = list(map(lambda x: list(map(lambda x: Cell(), range(width))), range(height)))
-    return screen
-
-# rules ( from wikipedia )
-#   Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-#   Any live cell with two or three live neighbours lives on to the next generation.
-#   Any live cell with more than three live neighbours dies, as if by overpopulation. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-# condensed Any live cell with two or three live neighbours survives.
-#   Any dead cell with three live neighbours becomes a live cell.
-#   All other live cells die in the next generation. Similarly, all other dead cells stay dead.
-def checkCells(screen: list[list[Cell]]):
+def check_cells(screen: list[list[Cell]]):
     death = []
     life = []
 
@@ -65,69 +42,79 @@ def checkCells(screen: list[list[Cell]]):
 
     return (life, death)
 
+def wrap_screen(screen, x, y):
+    if x == len(screen):
+        x = 0
+
+    if y == len(screen[0]):
+        y = 0
+    return (x, y)
+
 def kill(screen: list[list[Cell]], x, y): 
     screen[y][x].cellstate = CellState.DEAD
 
-    for neighbor_pos in neighboringPositions(x, y):
-        try:
-            screen[neighbor_pos[1]][neighbor_pos[0]].dec_neighbors()
-        except IndexError:
-            pass
+    for neighbor_pos in neighboring_positions(x, y):
+        nx = neighbor_pos[0]
+        ny = neighbor_pos[1]
+
+        (nx, ny) = wrap_screen(screen, nx, ny)
+
+        screen[ny][nx].dec_neighbors()
 
 def birth(screen: list[list[Cell]], x: int, y: int):
+    if x < 0 or x > len(screen[0]) or y < 0 or y > len(screen):
+        print("why is there a negative im going insane and will be exiting")
+        exit(-1)
+
+    print(f"{x}, {y}")
 
     if screen[y][x].cellstate == CellState.DEAD:
-        for neighbor_pos in neighboringPositions(x, y):
-            try:
-                screen[neighbor_pos[1]][neighbor_pos[0]].inc_neighbors()
-            except IndexError:
-                pass
+        for neighbor_pos in neighboring_positions(x, y):
+
+            (nx, ny) = wrap_screen(screen, neighbor_pos[0], neighbor_pos[1])
+
+            screen[ny][nx].inc_neighbors()
 
     screen[y][x].cellstate = CellState.ALIVE
 
 def purge(screen: list[list[Cell]], life: list[tuple[int, int]], death: list[tuple[int, int]]):
-
     for cellpos in death:
-        x = cellpos[0]
-        y = cellpos[1]
-        kill(screen, x, y)
+        kill(screen, cellpos[0], cellpos[1])
 
     for cellpos in life:
-        x = cellpos[0]
-        y = cellpos[1]
-        birth(screen, x, y)
+        birth(screen, cellpos[0], cellpos[1])
 
 
 def advance(screen):
-    (life, death) = checkCells(screen)
+    (life, death) = check_cells(screen)
     purge(screen, life, death)
-    os.system("clear") # demolishing windows with a single line. im sure theres a better way to do this
     drawScreen(screen)
 
+def create_glider(screen, x, y):
+    birth(screen, x,y)
+    birth(screen, x+1, y+1)
+    birth(screen, x+2,y+1)
+    birth(screen, x+2,y)
+    birth(screen, x+2,y-1)
+
 # constants
-HEIGHT = 50
-WIDTH = 100 
+HEIGHT = 10
+WIDTH = 10
+CHANCE = 0
 
 # program begin
-screen = createScreen(WIDTH, HEIGHT)
+screen = create_screen(WIDTH, HEIGHT)
 
-# flip flap ting
-birth(screen, 1, 1)
-birth(screen, 1, 2)
-birth(screen, 1, 3)
+if CHANCE != 0:
+    for _ in range(int((WIDTH * HEIGHT) / CHANCE)):
+        birth(screen, randint(0, WIDTH-1), randint(0, HEIGHT-1))
 
 # glider
-birth(screen, 5,5)
-birth(screen, 6,6)
-birth(screen, 7,6)
-birth(screen, 7,5)
-birth(screen, 7,4)
-
-
-for _ in range(int((WIDTH * HEIGHT) / 8)):
-    birth(screen, randint(0, WIDTH-1), randint(0, HEIGHT-1))
+create_glider(screen, 5, 5)
 
 drawScreen(screen)
 while True:
     input("press a key")
+    os.system("clear") # demolishing windows with a single line. im sure theres a better way to do this
     advance(screen)
+
